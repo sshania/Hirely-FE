2d7890ee11aea7826c383c2e9f49d0c29d6fd9af
marvel <marvelhubert@gmail.com>
2025-05-29 21:44:14 +0700

    Tambah Job Matchmaking Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import LoginPage from './Loginpage'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginPage from "./Loginpage"; 
+import JobMatchPage from "./JobMatchmakingPage"; 
 
 function App() {
-
-
   return (
-    <LoginPage></LoginPage>
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/jobmatchmaking" element={<JobMatchPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
